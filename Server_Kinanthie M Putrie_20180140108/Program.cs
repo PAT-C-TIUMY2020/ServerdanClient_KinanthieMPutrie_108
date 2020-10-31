3509bf75a7184acb8f4879153b01b0bfe9be2320
KinanthieMPutrie <kinanthieputrie@gmail.com>
2020-10-31 15:48:14 +0800

    Add project files.

---
+++ Server_Kinanthie M Putrie_20180140108/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P2_20180140108;
+using System.Windows.Forms;
+
+
+namespace Server_Kinanthie_M_Putrie_20180140108
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
